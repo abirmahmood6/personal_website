6b02eab7c35649386afd376eed4b4e34f3028ca6
abirmahmood6 <abirmahmood6@gmail.com>
2026-03-29 02:39:00 -0500

    setup project structure

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div className="homepage">
+      <Navbar />
+      <h1>Personal Website</h1>
+      <p>Under Construction 🚧</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <div className='logo'> Logo </div>
+            <ul className='nav-links'>
+                <span> About Me  </span>
+                <span> Projects </span>
+                <span> Resume </span>
+                <span> Contact </span>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
