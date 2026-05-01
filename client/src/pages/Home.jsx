8db8c4c17c63b2913eecf522280d15cb50317fe6
abirmahmood6 <abirmahmood6@gmail.com>
2026-05-01 02:54:11 -0500

    add my image and text on aboutMe

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,13 +6,12 @@ const Home = () => {
   return (
     <div className="homepage">
       <Navbar />
-      <div className="myImage">
-        <img src="" alt="" />
-      </div>
+
       {/* uaw GRID to design the two parts */}
-      <div className="aboutMe">
-        <h1>Personal Website</h1>
-        <p>Under Construction 🚧</p>
+      <h1>Personal Website</h1>
+      <div className='aboutMe'>
+        <p> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." </p>
+        <img src="myPic.png" alt="Abir Mahmood's Picture" className='myImage' />
       </div>
     </div>
   )
